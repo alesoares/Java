d059e81aa03fa15d48aef80fa7f1ab349e06dbb3
Alessandro Soares. <alesoares1975@gmail.com>
2025-11-10 17:36:21 -0300

    Criei uma classe de teste.

---
+++ Aula_03/src/br/com/alessandro/aula_03/teste.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package br.com.alessandro.aula_03;
+
+/**
+ * 
+ */
+public class teste {
+
+	/**
+	 * 
+	 */
+	public teste() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
